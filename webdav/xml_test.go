eefd68aa8cbff26f69acf78d6e673cd924131b62
LIN <chuanlin252@163.com>
2025-06-27 16:43:12 +0800

    update

--- webdav/xml_test.go
+++ webdav/xml_test.go
@@ -16,7 +16,7 @@ import (
 	"strings"
 	"testing"
 
-	ixml "golang.org/x/net/webdav/internal/xml"
+	ixml "github.com/general252/webdav-server/webdav/internal/xml"
 )
 
 func TestReadLockInfo(t *testing.T) {
